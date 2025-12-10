a865fafe04f82a933a9e4cdd96cdf9c7546b9e7c
Solirus <thorkasev@gmail.com>
2025-12-11 00:35:58 +0500

    homework

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,7 +2,17 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+number = 10
+result = 5 < number <= 19
+print(result)
+
+number = 5
+result = 5 < number <= 19
+print(result)
+
+number = 19
+result = 5 < number <= 19
+print(result)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
--- tasks/task6.py
+++ tasks/task6.py
@@ -2,8 +2,15 @@
 
 def solve():
 # Ниже пишите решение задачи
+a, b, c = 8, 6, 10
+a2, b2, c2 = a*a, b*b, c*c
+result = (a2 + b2 == c2) or (a2 + c2 == b2) or (b2 + c2 == a2)
+print(result)
 
-   
+a, b, c = 5, 6, 5
+a2, b2, c2 = a*a, b*b, c*c
+result = (a2 + b2 == c2) or (a2 + c2 == b2) or (b2 + c2 == a2)
+print(result)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
